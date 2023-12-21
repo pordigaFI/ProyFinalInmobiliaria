2f3e4fe8ee443fa5c4226b6e518c5664da5ef4f4
pordigaFI <79491737+pordigaFI@users.noreply.github.com>
2023-12-21 12:50:30 -0600

    Initial Commit

---
+++ InmobiliariaIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InmobiliariaIOS
+//
+//  Created by Porfirio Diaz on 21/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
